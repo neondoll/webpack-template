e66504e6f9ce57bf994fa415c236d86ded24960b
neondoll <bonni_1998@mail.ru>
2024-03-27 22:34:45 +0300

    Jest

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -6,7 +6,7 @@ test('basic test', () => {
   expect(result).toBe(4);
 });
 
-test('calculateTotal sum', () => {
+test('calculateTotal sum without discount', () => {
   const list = [
     {
       id: 456, name: 'Война и мир', count: 3, price: 400,
@@ -19,4 +19,19 @@ test('calculateTotal sum', () => {
   const result = calculateTotal(list);
 
   expect(result).toBe(2500);
+});
+
+test('calculateTotal sum with discount', () => {
+  const list = [
+    {
+      id: 456, name: 'Война и мир', count: 3, price: 401,
+    },
+    {
+      id: 77, name: 'JavaScript', count: 1, price: 1300,
+    },
+  ];
+
+  const result = calculateTotal(list, true);
+
+  expect(result).toBeCloseTo(2230.173);
 });
--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,3 +1,9 @@
-export function calculateTotal(purchases) {
-  return purchases.reduce((total, item) => total + item.count * item.price, 0);
+export function calculateTotal(purchases, applyDiscount) {
+  const sum = purchases.reduce((total, item) => total + item.count * item.price, 0);
+
+  if (applyDiscount) {
+    return sum * 0.891;
+  }
+
+  return sum;
 }
